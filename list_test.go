5bb1f7188f567dfe1baae550b77a786403407699
eidng8 <jackey.cheung@qq.com>
2024-10-30 17:54:54 +0800

    change to atlas versioned migration

--- list_test.go
+++ list_test.go
@@ -30,19 +30,23 @@ func Test_ListAdminArea_should_return_first_page(t *testing.T) {
 		PerPage:      10,
 		CurrentPage:  1,
 		LastPage:     last,
-		FirstPageUrl: "/admin-areas?page=1&per_page=10",
-		LastPageUrl:  fmt.Sprintf("/admin-areas?page=%d&per_page=10", last),
-		NextPageUrl:  "/admin-areas?page=2&per_page=10",
-		PrevPageUrl:  "",
-		Path:         "/admin-areas",
-		From:         1,
-		To:           10,
-		Data:         list,
+		FirstPageUrl: "http://127.0.0.1/admin-areas?page=1&per_page=10",
+		LastPageUrl: fmt.Sprintf(
+			"http://127.0.0.1/admin-areas?page=%d&per_page=10", last,
+		),
+		NextPageUrl: "http://127.0.0.1/admin-areas?page=2&per_page=10",
+		PrevPageUrl: "",
+		Path:        "http://127.0.0.1/admin-areas",
+		From:        1,
+		To:          10,
+		Data:        list,
 	}
 	bytes, err := jsoniter.Marshal(page)
 	assert.Nil(t, err)
 	expected := string(bytes)
-	req, _ := http.NewRequest(http.MethodGet, "/admin-areas", nil)
+	req, _ := http.NewRequest(
+		http.MethodGet, "http://127.0.0.1/admin-areas", nil,
+	)
 	engine.ServeHTTP(response, req)
 	assert.Equal(t, http.StatusOK, response.Code)
 	actual := response.Body.String()
@@ -64,19 +68,23 @@ func Test_ListAdminArea_should_return_fourth_page(t *testing.T) {
 		PerPage:      10,
 		CurrentPage:  4,
 		LastPage:     last,
-		FirstPageUrl: "/admin-areas?page=1&per_page=10",
-		LastPageUrl:  fmt.Sprintf("/admin-areas?page=%d&per_page=10", last),
-		NextPageUrl:  "/admin-areas?page=5&per_page=10",
-		PrevPageUrl:  "/admin-areas?page=3&per_page=10",
-		Path:         "/admin-areas",
-		From:         31,
-		To:           40,
-		Data:         list,
+		FirstPageUrl: "http://127.0.0.1/admin-areas?page=1&per_page=10",
+		LastPageUrl: fmt.Sprintf(
+			"http://127.0.0.1/admin-areas?page=%d&per_page=10", last,
+		),
+		NextPageUrl: "http://127.0.0.1/admin-areas?page=5&per_page=10",
+		PrevPageUrl: "http://127.0.0.1/admin-areas?page=3&per_page=10",
+		Path:        "http://127.0.0.1/admin-areas",
+		From:        31,
+		To:          40,
+		Data:        list,
 	}
 	bytes, err := jsoniter.Marshal(page)
 	assert.Nil(t, err)
 	expected := string(bytes)
-	req, _ := http.NewRequest(http.MethodGet, "/admin-areas?page=4", nil)
+	req, _ := http.NewRequest(
+		http.MethodGet, "http://127.0.0.1/admin-areas?page=4", nil,
+	)
 	engine.ServeHTTP(response, req)
 	assert.Equal(t, http.StatusOK, response.Code)
 	actual := response.Body.String()
@@ -98,20 +106,22 @@ func Test_ListAdminArea_should_return_fourth_page_5_per_page(t *testing.T) {
 		PerPage:      5,
 		CurrentPage:  4,
 		LastPage:     last,
-		FirstPageUrl: "/admin-areas?page=1&per_page=5",
-		LastPageUrl:  fmt.Sprintf("/admin-areas?page=%d&per_page=5", last),
-		NextPageUrl:  "/admin-areas?page=5&per_page=5",
-		PrevPageUrl:  "/admin-areas?page=3&per_page=5",
-		Path:         "/admin-areas",
-		From:         16,
-		To:           20,
-		Data:         list,
+		FirstPageUrl: "http://127.0.0.1/admin-areas?page=1&per_page=5",
+		LastPageUrl: fmt.Sprintf(
+			"http://127.0.0.1/admin-areas?page=%d&per_page=5", last,
+		),
+		NextPageUrl: "http://127.0.0.1/admin-areas?page=5&per_page=5",
+		PrevPageUrl: "http://127.0.0.1/admin-areas?page=3&per_page=5",
+		Path:        "http://127.0.0.1/admin-areas",
+		From:        16,
+		To:          20,
+		Data:        list,
 	}
 	bytes, err := jsoniter.Marshal(page)
 	assert.Nil(t, err)
 	expected := string(bytes)
 	req, _ := http.NewRequest(
-		http.MethodGet, "/admin-areas?page=4&per_page=5", nil,
+		http.MethodGet, "http://127.0.0.1/admin-areas?page=4&per_page=5", nil,
 	)
 	engine.ServeHTTP(response, req)
 	assert.Equal(t, http.StatusOK, response.Code)
--- main_test.go
+++ main_test.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"log"
 	"net/http/httptest"
 	"os"
 	"testing"
-	"time"
 
-	"entgo.io/ent/dialect"
-	entsql "entgo.io/ent/dialect/sql"
 	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
 	jitr "github.com/json-iterator/go"
 	"github.com/stretchr/testify/assert"
 
 	"eidng8.cc/microservices/admin-areas/ent"
-	_ "eidng8.cc/microservices/admin-areas/ent/runtime"
 )
 
 var jsoniter = jitr.ConfigCompatibleWithStandardLibrary
@@ -24,40 +17,18 @@ var jsoniter = jitr.ConfigCompatibleWithStandardLibrary
 func setupGinTest(tb testing.TB) (
 	*gin.Engine, *ent.Client, *httptest.ResponseRecorder,
 ) {
-	cfg := mysql.Config{
-		User:                 os.Getenv("DB_USER"),
-		Passwd:               os.Getenv("DB_PASSWORD"),
-		Net:                  "tcp",
-		Addr:                 os.Getenv("DB_ADDR"),
-		DBName:               os.Getenv("DB_NAME"),
-		MultiStatements:      true,
-		AllowNativePasswords: true,
-		ParseTime:            true,
-		Collation:            "utf8mb4_unicode_ci",
-		Loc:                  time.Local,
-	}
-	db, err := sql.Open(dialect.MySQL, cfg.FormatDSN())
-	if err != nil {
-		log.Fatalf("Failed to open MySQL connection: %s", err)
-	}
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(100)
-	db.SetConnMaxLifetime(time.Hour)
-	entClient := ent.NewClient(ent.Driver(entsql.OpenDB("mysql", db)))
+	assert.Nil(tb, os.Setenv("DB_DRIVER", "mysql"))
+	assert.Nil(tb, os.Setenv("DB_USER", "root"))
+	assert.Nil(tb, os.Setenv("DB_PASSWORD", "123456"))
+	assert.Nil(tb, os.Setenv("DB_HOST", "127.0.0.1:43306"))
+	assert.Nil(tb, os.Setenv("DB_NAME", "admin_areas"))
+	entClient := getEntClient()
 	tb.Cleanup(
 		func() {
-			if err := entClient.Close(); err != nil {
-				tb.Fatalf("Failed to close ent client: %s", err)
-			}
+			assert.Nil(tb, entClient.Close())
 		},
 	)
-
 	engine, err := NewEngine(gin.TestMode, entClient)
-	if err != nil {
-		assert.Nil(tb, err)
-	}
-
-	res := httptest.NewRecorder()
-
-	return engine, entClient, res
+	assert.Nil(tb, err)
+	return engine, entClient, httptest.NewRecorder()
 }
